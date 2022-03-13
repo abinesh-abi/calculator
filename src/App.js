9a3a6ef9e80bd0ca426c982988dfbfcf8c135e7d
abinesh-abi <abinesh1729@gmail.com>
2022-03-13 17:46:01 +0530

    styles edited

--- src/App.js
+++ src/App.js
@@ -3,9 +3,29 @@ import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+      <div className='calculator-grid'>
+        <div className="output">
+          <div className="previous">123</div>
+          <div className="current">123</div>
+        </div>
+        <button className="span-two">AC</button>
+        <button>DEL</button>
+        <button>/</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className='span-two'>=</button>
+      </div>
   );
 }
