aade677ce061792e32d9fe3d59844e149ca82dd4
abinesh-abi <abinesh1729@gmail.com>
2022-03-26 12:57:00 +0530

    button component added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useReducer } from 'react';
+import DigitButton from './DigitButton';
+import OperationButton from './OperationButton';
 import './style.css';
 
 export const ACTIONS = {
@@ -9,10 +11,27 @@ export const ACTIONS = {
   EVALUATE: 'evaluate',
 };
 
-function reduser(state, { type, payload }) {}
+function reduser(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      if (payload.digit === '0' && state.currentOperant === '0'){
+        return state
+      }
+      if (payload.digit === '.' && state.currentOperant.includes(".")) {
+        return state
+      }
+      return {
+        ...state,
+        currentOperant: `${state.currentOperant || ''} ${payload.digit}`,
+      };
+      case ACTIONS.CLEAR:
+        return {}
+  }
+
+}
 
 export default function App() {
-  const [{ currentOperant, previousOperant, operation }] = useReducer(
+  const [{ currentOperant, previousOperant, operation }, dispatch] = useReducer(
     reduser,
     {}
   );
@@ -21,27 +40,27 @@ export default function App() {
     <div className="calculator-grid">
       <div className="output">
         <div className="previous">
-          {previousOperant} {operation}{' '}
+          {previousOperant} {operation}
         </div>
         <div className="current">{currentOperant}</div>
       </div>
-      <button className="span-two">AC</button>
+      <button className="span-two" onClick={()=>dispatch({type:ACTIONS.CLEAR})}>AC</button>
       <button>DEL</button>
-      <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
+      <OperationButton operation="/" dispatch={dispatch} />
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
+      <DigitButton digit="." dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
     </div>
   );
